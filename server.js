084a31508fdb55bb3eda32f667556a7bf8bc3f0c
itsyaboikris <kristoff_riley@outlook.com>
2022-07-16 12:44:25 -0400

    multer setup

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const multer = require("multer")
+
 const express = require("express")
 const http = require("http")
 
@@ -5,13 +7,15 @@ const PORT = 3001
 const app = express()
 const server = http.createServer(app)
 
+const upload = multer ({dest: "uploads"})
+
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
 	res.render("index")
 })
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
 	res.send("Hello")
 })
 
